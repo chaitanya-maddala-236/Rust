d12d991a78286d2f9569562aa4907362b79625e4
chaitanya-maddala-236 <chaitanyaa.2006@gmail.com>
2025-09-21 22:55:59 +0530

    rust started

---
+++ src/new.rs
@@ -0,0 +1,24 @@
+fn greet(name: &str){
+    println!("hello {}",name);
+}
+
+fn add(a:i32,b:i32)->i32{
+    return a+b;
+}
+
+
+fn main(){
+    greet("chaitanya");
+    let sum=add(10,20);
+    println!("{}",sum);
+
+    let x=3;
+ {   println!("{}",x);
+}println!(
+    "{}",x
+)
+
+
+
+
+}
---
+++ src/ownership.rs
@@ -0,0 +1,7 @@
+fn main(){
+    let a=3;
+    let b=a;
+    let c=a.clone();
+    println!("{},{},{}",a,b,c);
+
+}
