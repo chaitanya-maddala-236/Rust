e51fe118f8352a6b1bae433f3c989c9d45cfcfa6
chaitanya-maddala-236 <chaitanyaa.2006@gmail.com>
2025-09-21 22:33:40 +0530

    rust started

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+fn say_hello(){println!("hello ji");}
+fn main() {
+
+    say_hello();
+    say_hello();
+    say_hello();
+    say_hello();
+    say_hello();
+
+    println!("Hello, world!");
+println!("hello ji");
+
+let name= "m.chaitanya";
+println!("my name is {}",name);
+let age:i32=34;
+let price: bool=true;
+println!("age {}",price);
+const year:i32=2000;
+println!("year {}",year);
+println!("{}",true||false);
+
+let a:i32=19;
+let b:i64=23;
+
+let day=1;
+match day{
+1|2|3|4|5=>println!("minday"),
+1=>println!("minday"),
+1=>println!("minday"),
+1=>println!("minday"),
+_=>println!("minday"),
+
+}
+// infinite loop
+
+
+
+for i in 1..10{
+    println!("for loop {}",i);
+}
+
+}
+
+
+// rust follows - snake_case 
